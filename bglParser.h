e9ef2c8c6bfcb8a6f2735aa992e7326c1543d735
Jim Fisher <jim@onyxring.com>
2026-03-08 01:16:27 -0600

    building beguile

--- bglParser.h
+++ bglParser.h
@@ -2,13 +2,13 @@
 #include <string>
 #include <sstream>
 #include <deque>
+#include <set>
 
 #include "types.h"
-#include "globals.h"
+#include "helpers.h"
 #include "token.h"
 #include "fileLexer.h"
 #include "i6Emitter.h"
-#include "parseTreeNode.h"
 #include "typeDef.h"
 
 using namespace std;
@@ -17,8 +17,6 @@ class bglParser {
     public:
         fileLexer file;    //what the parser reads from.  Tokens are produced by the filelexer.
         
-        //vector<typeDef*> globals;
-
         bglParser();        
         bool parseFile(std::string);    //the main entry point: given a file, read it in, parse it, and store it in the parse tree
         bool parsingError(std::string);   //called when there is an error, to output the error message and the place in the code where it appeared
@@ -32,22 +30,34 @@ class bglParser {
         
     private:
         std::deque<eCompileLanguage> compileLanguageStack;   //of course, these aren't really stacks, but we use them that way
-        std::deque<eCompileContext> compileContextStack;     
+        std::deque<eCompileContext> compileContextStack;
+        std::set<std::string> loadedFiles;  // tracks absolute paths of already-loaded files (include guard)
 
         bool processNextStatement(abstractObject& =emptyContainer); 
-        bool processDataType(token);
+        //bool processDataType(token);
         bool processParameterList(functionDef&);
 
-        bool processClassDeclaration(token);
-        bool processEnumDeclaration(token);
+        bool processClassDeclaration(token, bool, bool isExtend=false);
+        bool processEnumDeclaration(token, bool);
+        bool processBeguilerSettings();
         
-        bool processObjectDeclaration(token, token);
+        bool processObjectDeclaration(token, token, bool);
 
-        bool processVariableDeclaration(token, token, token, abstractObject& = emptyContainer);
-        bool processRoutineDeclaration(token, token, abstractObject& = emptyContainer);
+        bool processVariableDeclaration(token, token, token, abstractObject& = emptyContainer, bool = false, bool = false);
+        bool processRoutineDeclaration(token, token, abstractObject& = emptyContainer, bool = false, bool = false);
         bool processStatement(token, abstractObject& = emptyContainer);
-        bool processDirective(token);
-        
+        bool processDirective(token, abstractObject& = emptyContainer);
+
+        expression* parseExpression(token firstToken, std::vector<std::string> terminators, functionDef* func, statementBlock* body);
+        std::string resolveIdentifierType(std::string name, functionDef* func, statementBlock* body);
+        std::string qualifyIdentifier(std::string name, functionDef* func, statementBlock* body);
+        bool isTypeCompatible(std::string argType, std::string paramType);
+        void applyArgConversions(std::vector<expression*>& args, functionDef* fd);
+
+        objectDef* currentObject = nullptr;
+        classDef* currentClass = nullptr;    // set when parsing inside a class declaration
+        functionDef* currentFunc = nullptr;  // outermost function being parsed (not changed for nested if/while blocks)
+
 };
 
 extern bglParser parser;
